d0482b9b3f3524451fcb67daaa6c9ca63186680b
Nathanael Connell <158105404+NathanaelConnell@users.noreply.github.com>
2024-11-07 19:06:33 -0700

    Changed to be my own code
    There was one part i used chat gpt for.  It was because i ran into an error that was making my loop go infinite so lines 13-15 i got dirrectly for chat gpt

--- code.js
+++ code.js
@@ -1,44 +1,40 @@
 function quicksort(array) {
+    if (array.length <= 1) {
+        return array;
+    }
 
-    if (array.length < 2) return array;
+    let stack = [];
+    stack.push(0);
+    stack.push(array.length - 1);
 
-    // A stack to hold the start and end indexes of the subarrays
-    const stack = [];
-    stack.push({ start: 0, end: array.length - 1 });
-
-    while (stack.length > 0) {
-        const { start, end } = stack.pop();
-
-        // finds the pivot index and partitions
-        const pivotIndex = partition(array, start, end);
-
-        // If there are elements on the left side of the pivot, add the left subarray to the stack
-        if (pivotIndex - 1 > start) {
-            stack.push({ start: start, end: pivotIndex - 1 });
+    while( stack.length > 0) {
+        let right = stack.pop();
+        let left = stack.pop();
+        if (right - left <= 0) {
+            continue;
         }
-        
-        // If there are elements on the right side of the pivot, add the right subarray to the stack
-        if (pivotIndex + 1 < end) {
-            stack.push({ start: pivotIndex + 1, end: end });
+        let pivotElement = array[right];
+        let i = left - 1;
+
+        for (let j = left; j < right; j++) {
+            if (array[j] < pivotElement) {
+                i++;
+                let temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
         }
+        i++;
+        let temp = array[i];
+        array[i] = array[right];
+        array[right] = temp;
+
+        stack.push(left);
+        stack.push(i - 1);
+        stack.push(i + 1);
+        stack.push(right);
     }
 
     return array;
 }
 
-function partition(array, start, end) {
-    const pivot = array[end]; // Choose the last element as the pivot
-    let i = start - 1; // Index of the smaller element
-
-    for (let j = start; j < end; j++) {
-        if (array[j] < pivot) {
-            i++; // Increment the index of the smaller element
-            // Swap array[i] and array[j]
-            [array[i], array[j]] = [array[j], array[i]];
-        }
-    }
-
-    // Swap the pivot element with the element at index i + 1
-    [array[i + 1], array[end]] = [array[end], array[i + 1]];
-    return i + 1; // Return the partition index
-}
